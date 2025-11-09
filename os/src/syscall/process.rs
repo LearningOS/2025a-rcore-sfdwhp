d47e32fde054ed5fcc6cad30d7c71483d4b86052
sfdwhp <1023756234@qq.com>
2025-11-09 14:46:12 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -41,5 +41,17 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request{
+      0 => {
+       (unsafe { *(_id as *const usize) }) as isize
+      },
+      1 => {
+        unsafe{*(_id as *mut usize)=_data};
+        return 0;
+      },
+      2 => {
+        -1
+      },
+      _ => -1,
+    }
 }
